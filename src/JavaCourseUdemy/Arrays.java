602c525f5bf7c90cd1f0b7c4ea5ac60bc5b67ee3
grzehooW <grzehoo.Wrzosek@gmail.com>
2020-12-01 16:09:21 +0000

    Finding min number

--- src/JavaCourseUdemy/Arrays.java
+++ src/JavaCourseUdemy/Arrays.java
@@ -1,4 +1,27 @@
 package JavaCourseUdemy;
 
+import java.util.Scanner;
+
 public class Arrays {
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+
+        var numbers = new int[Integer.parseInt(scanner.nextLine())];
+
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = Integer.parseInt(scanner.nextLine());
+
+        var min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            var number = numbers[i];
+            if (numbers[i] < min) {
+                min = number;
+            }
+            System.out.println(min);
+
+
+        }
+    }
 }
